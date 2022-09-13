72f40ad31cef66d92466c20328efc8488ed3b8c0
Daniel <danielawde9@gmail.com>
2022-09-13 14:54:32 +0300

    fix react

---
+++ react-apps/mortage-calculator-app/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { FcHome } from "react-icons/fc";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <div
+      className="App container"
+      style={{ maxWidth: 500, margin: "1rem auto" }}
+    >
+      <h1 className="display-1 my-5">
+        <FcHome /> Mortgage Calculator{" "}
+      </h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
