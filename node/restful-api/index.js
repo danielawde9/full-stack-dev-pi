ecce863a82a7d8ebb7f2b7972bc3cafe0dbd9ae2
Daniel <danielawde9@gmail.com>
2022-09-13 19:44:36 +0300

    added finished node apps

---
+++ node/restful-api/index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+const routes = require('./routes/routes');
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
+
+//Here, this app.use takes two things. One is the base endpoint, and the other is the contents of the routes. Now, all our endpoints will start from '/api'.
+app.use('/api', routes)
+
+
+/* With Postman, to test HTTP post actions with a raw JSON data payload, select the raw option and set the following header parameters:
+
+Content-Type: application/json
+ */
+
+/*  "scripts": {
+    "start": "nodemon index.js"
+},
+
+This means that we can start our server using npm start, and it will run using the Nodemon package that  we previously installed. */
+
+
---
+++ node/restful-api/routes/routes.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const Model = require('../model/model')
+const router = express.Router();
+
+
+/* How to Write our Endpoints
+Now, let's write our endpoints here in this routes file. We will have five routes for the following actions:
+
+Posting data to Database.
+Getting all the data from the Database.
+Getting data based on the ID.
+Updating data based on the ID.
+Deleting data based on the ID.
+ */
+
+
+//Post Method
+//This router is taking the route as the first parameter. Then in the second parameter it's taking a callback.
+//  this function needs to be asynchronous to work. So, we will use async-await.
+
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+//Get all Method
+router.get('/getAll', async (req, res) => {
+    try {
+        const data = await Model.find();
+        res.json(data)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Get by ID Method
+router.get('/getOne/:id', async (req, res) => {
+    try{
+        const data = await Model.findById(req.params.id);
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Update by ID Method
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+//Delete by ID Method
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+module.exports = router
